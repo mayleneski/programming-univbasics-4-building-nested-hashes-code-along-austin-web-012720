15ebdba43784062108b0f2b032586447875cc33a
Maylene Poulsen <mayleneski@hotmail.com>
2019-12-02 16:14:24 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,8 +14,7 @@ def third_challenge
         :age => "15"
         :status => "alive"
         },
-      :hero_friends => 
-[
+      :hero_friends => [
         {
           :name => "Benvolio",
           :age => "17",
